66a3754765c3f88b022876381458154539b45b7d
Neeraj Kumar <neeraj.kumar32@wipro.com>
2020-04-18 16:30:11 +0530

    Revert "covid 19 related chanegs for tm"
    This reverts commit 0e65e9afa50fcbab368f79546e39431ad2db55f2

--- src/main/java/com/iemr/mmu/service/covid19/Covid19Service.java
+++
@@ -1,7 +0,0 @@
-package com.iemr.mmu.service.covid19;
-
-import com.google.gson.JsonObject;
-
-public interface Covid19Service {
-	public Long saveCovid19NurseData(JsonObject requestOBJ, String Authorization) throws Exception;
-}
