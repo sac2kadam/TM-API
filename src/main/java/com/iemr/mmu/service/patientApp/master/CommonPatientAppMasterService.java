3489a485e31eb73f4ce06a1ffebd8d3764678639
Neeraj Kumar (INDIA - HCIT) <NE298657@wipro.com>
2020-08-14 18:06:44 +0530

    merge

--- src/main/java/com/iemr/mmu/service/patientApp/master/CommonPatientAppMasterService.java
+++ src/main/java/com/iemr/mmu/service/patientApp/master/CommonPatientAppMasterService.java
@@ -10,11 +10,8 @@ public interface CommonPatientAppMasterService {
 	public String savechiefComplaintsData(String requestObj) throws Exception;
 
 	public Integer bookTCSlotData(String requestObj, String Authorization) throws Exception;
-<<<<<<< HEAD
 
 	public String getPatientEpisodeData(String requestObj) throws Exception;
-=======
-	
-	public String getMaster(Integer stateID );
->>>>>>> a020b425b5abba74d880d975b14ba8fb623e986f
+
+	public String getMaster(Integer stateID);
 }
