ea3069981839da3bb32cb13ba7c5863ab5505a91
Vidyaaa24 <vidyadevi.salunkhe@wipro.com>
2023-08-24 15:41:44 +0530

    changes made for swymed

--- src/main/java/com/iemr/tm/controller/videoconsultationcontroller/VideoConsultationController.java
+++ src/main/java/com/iemr/tm/controller/videoconsultationcontroller/VideoConsultationController.java
@@ -19,7 +19,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see https://www.gnu.org/licenses/.
 */
-package com.iemr.tm.controller.videoconsultation;
+package com.iemr.tm.controller.videoconsultationcontroller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
